a10fcb4835179af247d88af3eebeda049cc4b826
Joakim Olsen <joakim.olsen@bearingpoint.com>
2023-10-11 13:55:53 +0200

    fix error

--- analysis.py
+++ analysis.py
@@ -1,14 +1,10 @@
 import polars as pl
 from zipfile import ZipFile
-import os
-from dotenv import load_dotenv
 
 
-
-with ZipFile("most_played_songs.zip", 'r') as zip:
-    load_dotenv()
-    password = os.getenv("ZIP_PASSWORD")
-    f = zip.read(zip.filelist[0].filename, pwd=bytes(password, encoding="utf8"))
+with ZipFile("database/most_played_songs.zip", 'r') as zip:
+    filename = zip.filelist[0].filename
+    f = zip.read(filename)
     df = pl.read_csv(f, separator=",", encoding='ISO-8859-1')
 
 print("Sample stream data") 
@@ -55,10 +51,9 @@ print("\n\n\nMost popular songs within most danceable BPM")
 analysis4_getTop30SongsBy(df)
 
 
-with ZipFile("users.zip", 'r') as zip:
-    load_dotenv()
-    password = os.getenv("ZIP_PASSWORD")
-    f = zip.read(zip.filelist[0].filename, pwd=bytes(password, encoding="utf8"))
+with ZipFile("database/users.zip", 'r') as zip:
+    filename = zip.filelist[0].filename
+    f = zip.read(filename)
     df = pl.read_csv(f, separator=",", encoding='ISO-8859-1')
 
 
@@ -67,10 +62,12 @@ print(df)
 print(df.columns)
 
 def analysis5_GetUserAgesCount(df):
-    df_Binned = df.filter(pl.col("Age").is_not_nan()).with_columns(pl.col("Age").qcut(10).alias("AgeBin"))
+    df_Binned = df.filter(pl.col("Age").is_not_nan()).with_columns(pl.col("Age").cut([20, 30, 40, 50, 60]).alias("AgeBin"))
     df_Grouped = df_Binned.group_by("AgeBin").agg(pl.col("User ID").count().alias("NumUsers"))
     df_Binned = df_Grouped.sort("NumUsers", descending=True).limit(5)
     df = df_Binned
     print(df_Binned)
 
+
+print("\n\n\nMost popular age bins")
 analysis5_GetUserAgesCount(df)
